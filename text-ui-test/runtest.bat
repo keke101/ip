896d1e07ff8edd6c67378936dce1787bd7f2d712
keke101 <zack_1997@hotmail.com>
2020-09-15 21:09:33 +0800

    Update batch for automated testing

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 REM Clear bin directory
-if exist ..\bin del ..\bin\*
+if exist ..\bin rmdir /Q /S ..\bin
 
 REM create bin directory if it doesn't exist
 if not exist ..\bin mkdir ..\bin
@@ -10,10 +10,10 @@ REM delete output from previous run
 del ACTUAL.TXT
 
 REM Copy test_duke.txt for testing
-copy ./data/test_duke.txt ./data/duke.txt
+copy .\data\test_duke.txt .\data\duke.txt
 
 REM compile the code into the bin folder
-javac  -cp ..\src -Xlint:none -d ..\bin ../src/main/java/duke/*.java ../src/main/java/duke/task/*.java ../src/main/java/duke/exception/*.java
+javac  -cp ..\src -Xlint:none -d ..\bin ..\src\main\java\duke\*.java ..\src\main\java\duke\task\*.java ..\src\main\java\duke\exception\*.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
@@ -21,10 +21,10 @@ IF ERRORLEVEL 1 (
 REM no error here, errorlevel == 0
 
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -Dfile.encoding=UTF-8 -classpath ..\bin Duke < input.txt > ACTUAL.TXT
+java -Dfile.encoding=UTF-8 -classpath ..\bin duke.Duke < input.txt > ACTUAL.TXT
 
 REM Restore original duke.txt
-rm ./data/duke.txt
+del /Q .\data\duke.txt
 
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
