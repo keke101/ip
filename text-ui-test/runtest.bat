f4b8fefeb15d67f2b3c9338b48565b57f1e489f6
keke101 <zack_1997@hotmail.com>
2020-09-15 19:41:48 +0800

    Update automated testing

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -1,10 +1,16 @@
 @ECHO OFF
 
+REM Clear bin directory
+if exist ..\bin del ..\bin\*
+
 REM create bin directory if it doesn't exist
 if not exist ..\bin mkdir ..\bin
 
 REM delete output from previous run
 del ACTUAL.TXT
+
+REM Copy test_duke.txt for testing
+copy ./data/test_duke.txt ./data/duke.txt
 
 REM compile the code into the bin folder
 javac  -cp ..\src -Xlint:none -d ..\bin ../src/main/java/duke/*.java ../src/main/java/duke/task/*.java ../src/main/java/duke/exception/*.java
@@ -16,6 +22,9 @@ REM no error here, errorlevel == 0
 
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
 java -Dfile.encoding=UTF-8 -classpath ..\bin Duke < input.txt > ACTUAL.TXT
+
+REM Restore original duke.txt
+rm ./data/duke.txt
 
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
